d5355c4df1b600bc5845d64447107356f7c145b7
Vít Chrubasík <flow3rish@protonmail.com>
2020-12-02 17:31:09 +0100

    Initial commit

---
+++ src/main/java/cloud/chrubasik/ordersprocessingrestclient/OrdersprocessingRestclientApplication.java
@@ -0,0 +1,13 @@
+package cloud.chrubasik.ordersprocessingrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersprocessingRestclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersprocessingRestclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/chrubasik/ordersprocessingrestclient/OrdersprocessingRestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.chrubasik.ordersprocessingrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersprocessingRestclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
